8fdf38926b7455580bc5a51c8b32dff429d673b5
Michael Lobman <michael.lobman@gmail.com>
2022-03-27 10:39:17 -0400

    Complete assignment

--- ruby/find_shortest_string_recursive.rb
+++ ruby/find_shortest_string_recursive.rb
@@ -1,6 +1,15 @@
 def find_shortest_string_recursive(arr)
-  # type your code in here
+  return arr.first if arr.length == 1
+
+  result = find_shortest_string_recursive(arr[1..-1])
+
+  arr.first.length <= result.length ? arr.first : result
+
 end
+
+array = ["hi", "bye", "hello", "i"]
+
+find_shortest_string_recursive(array)
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'a'"
@@ -19,5 +28,19 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 end
 
+
+# Problem
+# Given an array of strings, write a recursive functoin
+# that returns the shortest string.
+# if there are two strings, reutnr the first to appear...
 # Please add your pseudocode to this file
+
+# take in an array
+
+# take length of first element
+
+# if it is shorter than the second element of array, call the function again and check 
+# third, save shorter, checking it against i+ 1
+
+
 # And a written explanation of your solution
